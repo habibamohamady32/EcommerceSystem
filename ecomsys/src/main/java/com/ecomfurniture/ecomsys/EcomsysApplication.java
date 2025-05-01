5d3050d82fa0de74837138dda3c3ee729745da28
habibamohamady32 <habibas032@gmail.com>
2025-05-01 10:06:06 +0300

    Initial commit

---
+++ ecomsys/src/main/java/com/ecomfurniture/ecomsys/EcomsysApplication.java
@@ -0,0 +1,13 @@
+package com.ecomfurniture.ecomsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomsysApplication.class, args);
+	}
+
+}
---
+++ ecomsys/src/test/java/com/ecomfurniture/ecomsys/EcomsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomfurniture.ecomsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
